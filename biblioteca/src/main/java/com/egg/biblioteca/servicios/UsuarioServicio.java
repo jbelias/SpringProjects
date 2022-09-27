6645000366d5da36fdaeb511d3f3ffd490bb8a1c
Juan Bautista Elias <bautielias01@gmail.com>
2022-09-26 21:13:39 -0300

    many changes of html and adding settings to put images at user profile

--- biblioteca/src/main/java/com/egg/biblioteca/servicios/UsuarioServicio.java
+++ biblioteca/src/main/java/com/egg/biblioteca/servicios/UsuarioServicio.java
@@ -1,12 +1,13 @@
-
 package com.egg.biblioteca.servicios;
 
+import com.egg.biblioteca.entidades.Imagen;
 import com.egg.biblioteca.entidades.Usuario;
 import com.egg.biblioteca.enums.Rol;
 import com.egg.biblioteca.exceptions.MiException;
 import com.egg.biblioteca.repositorios.UsuarioRepositorio;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
@@ -20,32 +21,70 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
-public class UsuarioServicio implements UserDetailsService{
-    
+public class UsuarioServicio implements UserDetailsService {
+
     @Autowired
     private UsuarioRepositorio usuarioRepositorio;
-    
+
+    @Autowired
+    private ImagenServicio imagenServicio;
+
     @Transactional
-    public void registrar(String nombre, String email, String password, String password2) throws MiException{
-        
+    public void registrar(MultipartFile archivo, String nombre, String email, String password, String password2) throws MiException {
+
         validar(nombre, email, password, password2);
-        
+
         Usuario usuario = new Usuario();
-        
+
         usuario.setNombre(nombre);
         usuario.setEmail(email);
-        
+
         usuario.setPassword(new BCryptPasswordEncoder().encode(password));
-        
+
         usuario.setRol(Rol.USER);
-        
+
+        Imagen imagen = imagenServicio.guardar(archivo);
+
+        usuario.setImagen(imagen);
+
         usuarioRepositorio.save(usuario);
     }
-    
-    public void validar(String nombre, String email, String password, String password2) throws MiException{
-        
+
+    @Transactional
+    public void actualizar(MultipartFile archivo, String idUsuario, String nombre, String email, String password, String password2) throws MiException {
+
+        validar(nombre, email, password, password2);
+
+        Optional<Usuario> respuesta = usuarioRepositorio.findById(idUsuario);
+        if (respuesta.isPresent()) {
+
+            Usuario usuario = respuesta.get();
+
+            usuario.setNombre(nombre);
+            usuario.setEmail(email);
+
+            usuario.setPassword(new BCryptPasswordEncoder().encode(password));
+
+            usuario.setRol(Rol.USER);
+
+            String idImagen = null;
+            if (usuario.getImagen() != null) {
+                idImagen = usuario.getImagen().getId();
+            }
+            Imagen imagen = imagenServicio.actualizar(archivo, idImagen);
+
+            usuario.setImagen(imagen);
+
+            usuarioRepositorio.save(usuario);
+        }
+
+    }
+
+    public void validar(String nombre, String email, String password, String password2) throws MiException {
+
         if (nombre.isEmpty() || nombre == null) {
             throw new MiException("El nombre no puede ser nulo o estar vacio.");
         }
@@ -62,25 +101,25 @@ public class UsuarioServicio implements UserDetailsService{
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        
+
         Usuario usuario = usuarioRepositorio.buscarPorEmail(email);
-        
+
         if (usuario != null) {
-            
+
             List<GrantedAuthority> permisos = new ArrayList();
-            
+
             GrantedAuthority p = new SimpleGrantedAuthority("ROLE_" + usuario.getRol().toString());
-            
+
             permisos.add(p);
-            
+
             ServletRequestAttributes attr = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
-            
+
             HttpSession session = attr.getRequest().getSession(true);
-            
-            session.setAttribute("usuariosession" , usuario); 
-            
+
+            session.setAttribute("usuariosession", usuario);
+
             return new User(usuario.getEmail(), usuario.getPassword(), permisos);
-        }else{
+        } else {
             return null;
         }
     }
